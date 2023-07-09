80cb8eba850565af6c49d3f37dfded326e14d56b
Adrian <105925275+adicsolus@users.noreply.github.com>
2023-07-09 12:04:59 +0200

    Update ShoperController.php

--- src/Controller/ShoperController.php
+++ src/Controller/ShoperController.php
@@ -35,25 +35,24 @@ class ShoperController extends AbstractController
     #[Route('/api/v1/products/descriptions', name: 'api_v1_descriptions')]
     public function descriptions(Request $request, ShoperDesc $shoperDesc): JsonResponse
 {
-    // Pobranie danych z żądania POST
     if ($request->isMethod('POST')) {
         $productIdFromShoper = $request->request->get('product_id');
 
-        // Pobranie nazwy produktu z pliku CSV na podstawie ID produktu z Shoper
+
         $productName = $this->getProductNameFromCSV($productIdFromShoper);
 
-        // Pobranie ID produktu z pliku CSV na podstawie nazwy produktu
+
         $productIdFromCSV = $this->getProductIdFromCSV($productName);
 
-        // Sprawdzenie, czy ID produktu z Shoper i z pliku CSV się zgadzają
+
         if ($productIdFromShoper != $productIdFromCSV) {
             throw new \Exception('Product ID mismatch');
         }
 
-        // Generowanie opisu na podstawie fragmentu nazwy produktu
+
         $opis = $shoperDesc->getDescription($productName);
 
-        // Wyszukiwanie dopasowania opisu do fragmentu nazwy
+
         foreach ($opis as $fragmentNazwy => $opisProduktu) {
             if (strpos($productName, $fragmentNazwy) !== false) {
                 $description = "<p>$opisProduktu</p>";
@@ -61,7 +60,7 @@ class ShoperController extends AbstractController
             }
         }
 
-        // Wysłanie opisu do Shoper za pomocą API
+
         $accessToken = $this->getAccessToken();
 
         $client = HttpClient::create();
@@ -92,14 +91,14 @@ private function getProductNameFromCSV(int $productIdFromShoper): string
     $csvFile = 'nazwa_pliku.csv';
     $csvReader = new CsvReader($csvFile);
 
-    // Przeszukaj plik CSV w poszukiwaniu odpowiedniego identyfikatora produktu z Shoper
+
     foreach ($csvReader as $row) {
         if ($row[0] == $productIdFromShoper) {
             return $row[1]; // Pobierz nazwę produktu
         }
     }
 
-    // Jeśli nie znaleziono produktu o podanym identyfikatorze, zwróć pusty ciąg znaków lub obsłuż błąd
+
     throw new \Exception('Product not found');
 }
 
@@ -108,13 +107,13 @@ private function getProductIdFromCSV(string $productName): int
     $csvFile = 'nazwa_pliku.csv';
     $csvReader = new CsvReader($csvFile);
 
-    // Przeszukaj plik CSV w poszukiwaniu odpowiedniej nazwy produktu
+
     foreach ($csvReader as $row) {
         if ($row[1] == $productName) {
             return $row[0]; // Pobierz ID produktu
         }
     }
 
-    // Jeśli nie znaleziono produktu o podanej nazwie, zwróć 0 lub obsłuż błąd
+
     throw new \Exception('Product not found');
 }
